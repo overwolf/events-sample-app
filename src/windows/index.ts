6aeb9d8c39eafa6b49f0f82b4879e50f976b4b91
EdoMathiasOW <edo.mathias@overwolf.com>
2024-07-02 09:05:44 +0300

    Removed experiment logging service

--- src/windows/index.ts
+++ src/windows/index.ts
@@ -1,6 +1,16 @@
 import 'reflect-metadata';
 import { container, inject, injectable } from 'tsyringe';
 import gameData from '../overwolf-platform/config/game-data';
+import { CommunicationHostService } from '../overwolf-platform/services/communication-host-service';
+import { GameDetectionService } from '../overwolf-platform/services/game-detection-service';
+import { GEPService } from '../overwolf-platform/services/gep-service';
+import { SettingsService } from '../overwolf-platform/services/settings-service';
+import { WindowManagerService } from '../overwolf-platform/services/window-manager.service';
+import {
+  CommunicationBustHostPayload,
+  CommunicationHostServiceBase,
+  CommunicationHostToken,
+} from '../types/services/communication-host-service-base';
 import {
   GameClosedPayload,
   GameDetectionServiceBase,
@@ -8,32 +18,15 @@ import {
   GameLaunchedPayload,
   PostGamePayload,
 } from '../types/services/game-detection-service-base';
-import { LoggingService } from '../overwolf-platform/services/logging-service';
-import { GEPService } from '../overwolf-platform/services/gep-service';
-import { CommunicationHostService } from '../overwolf-platform/services/communication-host-service';
-import { GameDetectionService } from '../overwolf-platform/services/game-detection-service';
-import { WindowManagerService } from '../overwolf-platform/services/window-manager.service';
-import { IN_GAME_WINDOW } from '../constants/window-names';
-import {
-  CommunicationBustHostPayload,
-  CommunicationHostServiceBase,
-  CommunicationHostToken,
-} from '../types/services/communication-host-service-base';
+import { GEPServiceBase, GEPToken } from '../types/services/gep-service-base';
+import { SettingsToken } from '../types/services/settings-service-base';
 import {
   WindowManagerServiceBase,
   WindowManagerToken,
 } from '../types/services/window-manager-service-base';
-import { GEPServiceBase, GEPToken } from '../types/services/gep-service-base';
-import {
-  LoggingToken,
-  LoggingServiceBase,
-} from '../types/services/logging-service-base';
-import { SettingsToken } from '../types/services/settings-service-base';
-import { SettingsService } from '../overwolf-platform/services/settings-service';
 
 container.registerSingleton(GEPToken, GEPService);
 container.registerSingleton(SettingsToken, SettingsService);
-container.registerSingleton(LoggingToken, LoggingService);
 container.registerSingleton(GameDetectionToken, GameDetectionService);
 container.registerSingleton(WindowManagerToken, WindowManagerService);
 container.registerSingleton(CommunicationHostToken, CommunicationHostService);
@@ -46,8 +39,6 @@ export class IndexController {
   public constructor(
     @inject(GEPToken)
     private readonly gepService: GEPServiceBase,
-    @inject(LoggingToken)
-    private readonly loggingService: LoggingServiceBase,
     @inject(GameDetectionToken)
     private readonly gameDetectionService: GameDetectionServiceBase,
     @inject(WindowManagerToken)
@@ -56,7 +47,6 @@ export class IndexController {
     private readonly communicationBusHostService: CommunicationHostServiceBase,
   ) {
     this.communicationBusHostService.initializeCommunicationBusHost();
-    this.loggingService.init(IN_GAME_WINDOW);
     this.init();
   }
 
@@ -98,7 +88,6 @@ export class IndexController {
   private onGameStart(gameLaunch: GameLaunchedPayload) {
     console.log(`Game was launched: ${gameLaunch.name} ${gameLaunch.id}`);
     // Rechecks and saves the current GEP version, for logs backup
-    this.loggingService.reCheckGEPVersion();
     // Get the configured data for the launched game
     const gameConfig = gameData[gameLaunch.id];
     // If the detected game is configured for events
@@ -118,13 +107,6 @@ export class IndexController {
       // Close the in-game window
       this.windowManagerService.closeWindow(this.inGameName, (window) => {
         this.communicationBusHostService.windowDisconnected(window.windowName);
-        this.loggingService.backupLog(
-          `${gameClosed.name}/${new Date()
-            .toISOString()
-            .replaceAll(/[:]/gm, '-')
-            .replace('T', '--')
-            .replace(/\.\d+Z/gm, '')}`,
-        );
       });
       // Run game closed cleanup of the gep service
       this.gepService.onGameClosed();
